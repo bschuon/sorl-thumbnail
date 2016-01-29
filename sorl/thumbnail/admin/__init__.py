65ab170e129db70a57a47fbc5d3d0e8e9bf566c3
Bob Schuon <bob.schuon@loungelizard.com>
2016-01-29 16:27:33 -0500

    Missed a line or two.

--- sorl/thumbnail/admin/__init__.py
+++ sorl/thumbnail/admin/__init__.py
@@ -2,8 +2,9 @@ try:
     from django.forms import ClearableFileInput
 except ImportError:
     from .compat import AdminImageMixin
+    AdminInlineImageMixin = AdminImageMixin # backwards compatibility
 else:
     pass
     #from .current import AdminImageMixin
 
-AdminInlineImageMixin = AdminImageMixin # backwards compatibility
+#AdminInlineImageMixin = AdminImageMixin # backwards compatibility
